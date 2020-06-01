a4ce49b5c00ef68597c83ada138f0144dc5d86eb
kmarks2013 <kmarks2013@gmail.com>
2020-05-31 23:09:08 -0400

    adds login form copied from blog post

---
+++ src/components/LoginForm.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
+import { Formik } from 'formik'
+
+const LoginForm = () => {
+    return (
+        <Formik
+        initialValues={{username:'', password:''}}
+        onSubmit={(values) => console.log('submitted', values)}
+    >
+        {({handleChange, handleSubmit, values})=> (
+        <View style={styles.form}>
+            <Text style={styles.label}>Username:</Text>
+            <TextInput
+                style={styles.input}
+                value={values.username}
+                onChangeText={handleChange('username')}
+            />
+            <Text style={styles.label}>Password:</Text>
+            <TextInput
+                style={styles.input}
+                value={values.password}
+                onChangeText={handleChange('password')}
+                secureTextEntry={true}
+            />
+            <Button
+                title="Submit"
+                onPress={handleSubmit}
+            />
+        </View>
+         )}
+    </Formik>
+    )
+}
+
+export default LoginForm
+
+const styles = StyleSheet.create({
+    input: {
+          margin: 15,
+          padding: 5,
+          borderWidth: 1
+    },
+    form: {
+        borderColor: 'black',
+        borderWidth: 1
+    },
+    label: {
+        marginLeft: 5
+    }
+  })
