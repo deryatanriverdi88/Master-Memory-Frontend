60f36dea307b0d8a2c8debf99b7da6ba77de07c4
kmarks2013 <kmarks2013@gmail.com>
2020-05-31 23:10:03 -0400

    imports login for and rdners on landing page

--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Text, View, TextInput, StyleSheet, Button } from 'react-native'
+import LoginForm from '../components/LoginForm'
 
 export default class LandingScreen extends Component {
     render() {
         return (
             <View>
                 <Text style={styles.login}> Login </Text>
+                <LoginForm />
                 <Button title="Dont have an Account? Sign up here!" />
             </View>
         )
