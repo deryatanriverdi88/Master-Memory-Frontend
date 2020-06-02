2a1f4d3cd3c16700fde2a01ddd8fc6d05b0c49a9
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-02 18:36:48 -0400

    adds SignUoForm file

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, TextInput, View, StyleSheet, Button } from 'react-native'
+import { Formik } from 'formik'
+
+export default class SignUpForm extends Component {
+    render() {
+        return (
+          <View>
+              <Text>Sign Up Page</Text>
+          </View>
+        )
+    }
+}
