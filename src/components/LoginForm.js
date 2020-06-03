c9ff58297ae0c12de77e0075acd8518225151bc9
kmarks2013 <kmarks2013@gmail.com>
2020-06-02 23:58:09 -0400

    imports touchable opacity to update loginform button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
+import { View, Text, Button, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 import { Formik } from 'formik'
 
 const LoginForm = ({navigate}) => {
