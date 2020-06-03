082ed47564ce2464bf5658b82c2d618a1c2228b5
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-02 21:54:23 -0400

    deletes import of button in LandingScreen

--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, TextInput, StyleSheet, Button } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import LoginForm from '../components/LoginForm'
 import SignUpForm from '../components/SignUpForm'
 
