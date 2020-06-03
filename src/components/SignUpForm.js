f7d4bcd1ea84595ca7c0cea1b544b7804000bb5f
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-02 21:51:19 -0400

    adds button to signup form to go Login form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,54 +1,59 @@
 import React, { Component } from 'react'
-import { Text, TextInput, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, TextInput, View, StyleSheet, TouchableOpacity, Button } from 'react-native'
 import { Formik } from 'formik'
 
 export default class SignUpForm extends Component {
     render() {
         return (
-            <Formik
-             initialValues={{name: "",
-                            age: "",
-                            username: "",
-                            email: "",
-                            password: ""}}
-             onSubmit={(values) => console.log('submitted', values)}>
-                {({handleChange, handleSubmit, values}) => (
-                 <View style={styles.form}>
-                    <Text style={styles.label}>Name:</Text>
-                    <TextInput
-                    style={styles.input}
-                    value={values.name}
-                    onChangeText={handleChange("name")}/>
-                    <Text style={styles.label}>Age:</Text>
-                    <TextInput
-                    style={styles.input}
-                    value={values.age}
-                    onChangeText={handleChange("age")}/>
-                    <Text style={styles.label}>Username:</Text>
-                    <TextInput
-                    style={styles.input}
-                    value={values.username}
-                    onChangeText={handleChange("username")}/>
-                    <Text style={styles.label}>Email:</Text>
-                    <TextInput
-                    style={styles.input}
-                    value={values.email}
-                    onChangeText={handleChange("email")}/>
-                    <Text style={styles.label}>Password:</Text>
-                    <TextInput
-                    style={styles.input}
-                    value={values.password}
-                    onChangeText={handleChange("password")}
-                    secureTextEntry={true}/>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={handleSubmit}>
-                            <Text style={styles.buttonText}>Submit</Text>
-                    </TouchableOpacity>
-                 </View>
+            <View>
+                <Formik
+                initialValues={{name: "",
+                                age: "",
+                                username: "",
+                                email: "",
+                                password: ""}}
+                onSubmit={(values) => console.log('submitted', values)}>
+                    {({handleChange, handleSubmit, values}) => (
+                    <View style={styles.form}>
+                        <Text style={styles.label}>Name:</Text>
+                        <TextInput
+                        style={styles.input}
+                        value={values.name}
+                        onChangeText={handleChange("name")}/>
+                        <Text style={styles.label}>Age:</Text>
+                        <TextInput
+                        style={styles.input}
+                        value={values.age}
+                        onChangeText={handleChange("age")}/>
+                        <Text style={styles.label}>Username:</Text>
+                        <TextInput
+                        style={styles.input}
+                        value={values.username}
+                        onChangeText={handleChange("username")}/>
+                        <Text style={styles.label}>Email:</Text>
+                        <TextInput
+                        style={styles.input}
+                        value={values.email}
+                        onChangeText={handleChange("email")}/>
+                        <Text style={styles.label}>Password:</Text>
+                        <TextInput
+                        style={styles.input}
+                        value={values.password}
+                        onChangeText={handleChange("password")}
+                        secureTextEntry={true}/>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={handleSubmit}>
+                                <Text style={styles.buttonText}>Submit</Text>
+                        </TouchableOpacity>
+                    </View>
 
-                )}
-            </Formik>
+                    )}
+                </Formik>
+                <Button
+                onPress={() => this.props.navigation.navigate("Login")}
+                title="Do you have an Account? Login here!" />
+            </View>
         )
     }
 }
