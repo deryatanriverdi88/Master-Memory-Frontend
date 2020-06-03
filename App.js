06129176a4fe3888c1e3d2c871705a00a74df823
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-02 21:53:28 -0400

    deletes imports of styleSheet, Text, View in app.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
-import { StyleSheet, Text, View } from 'react-native';
 import LandingScreen from './src/screens/LandingScreen';
 import SignUpForm from './src/components/SignUpForm'
 
