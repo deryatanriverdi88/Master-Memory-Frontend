1314dc48d99484d01c79d20cb080940e74cfc793
kmarks2013 <kmarks2013@gmail.com>
2020-06-07 17:05:59 -0400

    adds redux folder and store.js with intial setup

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import thunk from 'redux-thunk'
+
+const reducers = combineReducers({
+
+})
+
+export default createStore(reducers, applyMiddleware(thunk))
