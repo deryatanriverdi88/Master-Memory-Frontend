13e93ffb725616e870d1f29626321bb30ca8cfd7
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-08 01:10:51 -0400

    comments out console logs, and corrects typo in currentUserReducer

--- src/redux/reducers/currentUserReducer.js
+++ src/redux/reducers/currentUserReducer.js
@@ -1,10 +1,12 @@
 export default (state = {user:{}, errors: [] }, {type, payload}) => {
     switch (type){
         case 'SET_USER':
+            // console.log(payload)
             return {...state, user: payload, errors: []};
         case 'CLEAR_USER':
             return {};
-        case "SET_ERROR":
+        case "SET_ERRORS":
+            // console.log(payload)
             return {...state, errors:payload}
         default:
             return state;
