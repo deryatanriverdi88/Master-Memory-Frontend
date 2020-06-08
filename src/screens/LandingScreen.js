cd3dcb79afcfde2213893ec5e9189e2eec1d7bd0
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-08 01:14:15 -0400

    imports  useEffect, useSelector, useDispatch, HomeScreen, userActions for LandingScreen

--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import LoginForm from '../components/LoginForm'
+import { useSelector, useDispatch} from 'react-redux'
+import HomeScreen from './HomeScreen'
+import userActions from '../redux/actions/userActions'
+
 
 const LandingScreen = ({navigation}) => {
     return (
