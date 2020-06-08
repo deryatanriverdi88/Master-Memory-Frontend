9e346d784fe86114a4d0bb357f39da175de77a0d
kmarks2013 <kmarks2013@gmail.com>
2020-06-07 20:13:40 -0400

    updates currentUserReducer

--- src/redux/reducers/currentUserReducer.js
+++ src/redux/reducers/currentUserReducer.js
@@ -1,9 +1,11 @@
-export default (state = {}, {type, payload}) => {
+export default (state = {user:{}, errors: [] }, {type, payload}) => {
     switch (type){
         case 'SET_USER':
-            return payload;
+            return {...state, user: payload, errors: []};
         case 'CLEAR_USER':
             return {};
+        case "SET_ERROR":
+            return {...state, errors:payload}
         default:
             return state;
     }
